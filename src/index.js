c378ec7881b9a7d3053356ea97f4d83b26618869
Hoang Son <soonhoang14@gmail.com>
2022-01-19 19:53:22 +0700

    last-push

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
-import App from './App';
-import store from './app/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import App from "./App";
+import store from "./app/store";
+import { Button } from "@mui/material";
+
+const notistackRef = React.createRef();
+const onClickDismiss = (key) => () => {
+  notistackRef.current.closeSnackbar(key);
+};
+
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <Provider store={store}>
-  <SnackbarProvider maxSnack={3} anchorOrigin={{vertical: 'top' , horizontal: 'right'}}>
-    <App />
-    </SnackbarProvider>
-  </Provider>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Provider store={store}>
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          iconVariant={{
+            success: "✅",
+            error: "✖️",
+            warning: "⚠️",
+            info: "ℹ️",
+          }}
+          ref={notistackRef}
+          action={(key) => (
+            <Button style={{color: 'red'}} onClick={onClickDismiss(key)}>Close</Button>
+          )}
+        >
+          <App />
+        </SnackbarProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
